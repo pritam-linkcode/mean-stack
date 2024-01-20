cac66c2d5ec1a32b9d31d74c414c7544e13af167
Pritam Kamble <pritam.kamble@linkcode.in>
2024-01-20 12:58:14 +0530

    Angular structure updated

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { UsersComponent } from "./users/users.component";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterOutlet } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { AppRoutingModule } from "./app-routing.module";
+import { BrowserModule } from "@angular/platform-browser";
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        UsersComponent,
+    ],
+    imports: [
+        CommonModule,
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule, 
+        FormsModule,
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
